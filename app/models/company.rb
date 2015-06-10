90d7882d9e451afcbd5bca187289530c8b5248ca
rostyslav-m <ros55@ukr.net>
2015-06-10 17:26:48 +0300

    add stats for companies

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,4 +2,14 @@ class Company < ActiveRecord::Base
   has_many :operations
 
   validates_presence_of :name
+
+
+  def total_amount
+    operations.to_a.sum { |operation| operation.amount }
+  end
+
+  def accepted_operations 
+    Operation.group(:status).count['accepted']
+  end
+
 end
