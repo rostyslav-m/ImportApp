d9bc7df44296d5dfa315d41defa954fb21d45323
rostyslav-m <ros55@ukr.net>
2015-06-14 20:31:16 +0300

    add export into csv

--- app/controllers/operations_controller.rb
+++ app/controllers/operations_controller.rb
@@ -5,6 +5,11 @@ class OperationsController < ApplicationController
   def index
     @search = Operation.search(params[:q])
     @operations = @search.result
+      respond_to do |format|
+        format.html
+        format.csv { send_data @operations.to_csv }
+#        format.xls # { send_data @products.to_csv(col_sep: "\t") }
+      end
   end
 
   def show
--- app/models/operation.rb
+++ app/models/operation.rb
@@ -6,4 +6,23 @@ class Operation < ActiveRecord::Base
 #  validates_presence_of :invoice_num, :invoice_date, :amount, :operation_date, :kind, :status
 #  validates_numericality_of :amount, greater_than: 0
 #  validates_uniqueness_of :invoice_num
+
+  def self.to_csv
+    CSV.generate do |csv|
+      csv << ["company", "invoice_num", "invoice_date", "operation_date", "amount", "reporter", "notes", "status", "kind"]
+        all.each do |operation|
+          csv << [operation.company.name, operation.invoice_num, operation.invoice_date, operation.operation_date, operation.amount, operation.reporter, operation.notes, operation.status, operation.kind, operation.categories.collect { |o| o.name }.join(',') ]
+        end
+    end
+  end
+
+
+#    CSV.generate do |csv|
+#      csv << column_names
+#      all.each do |operation|
+#        csv << operation.attributes.values_at(*column_names)
+#      end
+#    end
+#  end
+
 end
